d8cfa41caa40e9c88d4d4cf83fd93eb00a5a8fd7
Menciajp <menciajp.27@gmail.com>
2024-02-12 15:16:23 -0300

    1era parte responsive

--- js/menu.js
+++ js/menu.js
@@ -1,8 +1,13 @@
 document.addEventListener('DOMContentLoaded', function () {
     document.getElementById('boton-menu').addEventListener('click', function () {
         var menu = document.getElementById('menu');
+        var scrollbar = document.documentElement.style;
+        // Cambiar el estilo de la barra de desplazamiento al abrir el menú
+        scrollbar.overflowY = 'hidden'; // Oculta la barra de desplazamiento vertical
         if (menu.style.right === '0px') {
             menu.style.right = '-200px';
+            document.documentElement.style.opacity = '0';
+            setTimeout(() => { scrollbar.overflowY = ''; }, 300);
         } else {
             menu.style.right = '0px';
         }
@@ -12,9 +17,12 @@ document.addEventListener('DOMContentLoaded', function () {
         var menu = document.getElementById('menu');
         var botonMenu = document.getElementById('boton-menu');
         var target = event.target;
-
         if (!menu.contains(target) && target !== botonMenu) {
             menu.style.right = '-200px';
+            document.documentElement.style.opacity = '1';
+            setTimeout(() => { 
+                document.documentElement.style.overflowY = 'auto'; // Restablece después de la animación
+            }, 300);
         }
     });
 });
